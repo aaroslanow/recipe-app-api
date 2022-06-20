0e06041e5809d911910aa58aa8e81ba58446e8f7
Adam <adam.aroslanow@gmail.com>
2022-06-20 23:28:21 +0200

    added db dependencies

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""sample tests"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        result = calc.add(5,6)
+
+        self.assertEquals(result,11)
+
+    def test_substract_numbers(self):
+        result = calc.subtract(10,15)
+        self.assertEquals(result,5)
